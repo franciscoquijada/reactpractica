6aeb77135b2f12474ae0de5c8244a32cc62fbdc1
fquijada <franskferdinand@hotmail.com>
2020-09-26 23:49:59 -0300

    Creacion de formulairo

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,46 @@
-import React, {Fragment} from 'react';
+import React, {useState, Fragment} from 'react';
 
 const Form = () => {
+    const [data, setData] = useState({nombre: '', apellido: ''});
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(data.nombre + ' ' + data.apellido);
+    }
+    
+    const handleInputChange = (event) => {
+        setData({
+            ...data,
+            [event.target.name] : event.target.value,
+        });
+    }
+
     return (
         <Fragment>
-            <h1>Formulario 1</h1>
+            <div className="container text-center">
+                <h1>Formulario</h1>
+                <form onSubmit={handleSubmit} className="row mt-5">
+                    <div className="col-md-6">
+                            <input 
+                                onChange={handleInputChange} 
+                                type="text"
+                                name="nombre"
+                                placeholder="Ingresa tu nombre"
+                                className="form-control"
+                                />
+                        </div>
+                        <div className="col-md-6">
+                            <input
+                                onChange={handleInputChange} 
+                                type="text"
+                                name="apellido"
+                                placeholder="Ingresa tu apellido"
+                                className="form-control"
+                                />
+                    </div>
+                    <button className="btn btn-primary my-2" type="submit">Enviar</button>
+                </form>
+           </div>
         </Fragment>
     );
 }
